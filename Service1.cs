9edc5625543a861b805e94dc075ee777ce21b9e9
user <user@DESKTOP-EMVQO46>
2024-10-21 08:35:15 +0300

    Add project files.

---
+++ Service1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itlikHainlik
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            System.Threading.Thread.Sleep(30000);
+            System.IO.File.Delete(@"C:\aa\aa.txt");
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
